59c011d896f350c371da2d7e03bc76dd7dc3f86d
Kulukulu <eexma98@nottingham.ac.uk>
2021-09-09 12:53:57 +0100

    added security auth stuff and task, tasklist api endpoints

--- code/app.py
+++ code/app.py
@@ -1,9 +1,23 @@
 #imports
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_jwt import JWT
+#imports from packages within the project folder
 
-#configs
+#Flask Configurations
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'kulu'
+api = Api(app)
 
+jwt = JWT(app, authenticate, identity) #auth endpoint
 #create db with sqlalchemy
 
 #endpoints
 
 #run app with if name main
+if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
+    app.run(port=5000, debug=True)
