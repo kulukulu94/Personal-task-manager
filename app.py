57c5dba252fb9ba0734c31d3f5e80ddbfa8e9881
Kulukulu <eexma98@nottingham.ac.uk>
2021-09-28 19:38:05 +0100

    Added invalid token callback error msg + fixed typo on missing token call back

--- app.py
+++ app.py
@@ -31,14 +31,20 @@ def add_claims_to_jwt(identity):
 
 @jwt.token_in_blocklist_loader
 def check_if_token_in_blacklist(jwt_header, jwt_payload):
-        
         return jwt_payload['jti'] in BLACKLIST #jaw_payload['sub'] the id should be fetched from a db
 
-@jwt.unaurhorized_loader
-def missing_token_callback():
+@jwt.unauthorized_loader
+def missing_token_callback(error):
     return jsonify({
         'description': 'request has not authorization token',
         'error': 'authorization_required'
+    }), 401
+
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+    return jsonify({
+        'description': 'Signature verfication failed',
+        'error': 'invalid_token'
     }), 401
 
 @jwt.revoked_token_loader
